aa1b7e0bbd1cbfc35d3378aad8879e7c627a64ab
Awad <34748292+Awaddd@users.noreply.github.com>
2023-07-09 18:04:45 +0100

    Now caching pokemon details api response

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { pokemonUrl } from "../network/api";
+import localforage from "localforage";
 import { Pokemon, PokemonDTO } from "../types/pokemon";
 import { pokemonMapper } from "../network/pokemon-mapper";
 
@@ -11,8 +12,18 @@ export const usePokemon = (id: number) => {
     if (!id) return;
 
     const fetchData = async () => {
-      const { data } = await axios.get<PokemonDTO>(`${pokemonUrl}${id}`);
-      setPokemon(pokemonMapper(data));
+      let temp: Pokemon;
+
+      const cached = await localforage.getItem<Pokemon>(`pokemon-${id}`);
+
+      if (cached) temp = cached;
+      else {
+        const { data } = await axios.get<PokemonDTO>(`${pokemonUrl}${id}`);
+        temp = pokemonMapper(data);
+        await localforage.setItem(`pokemon-${id}`, temp);
+      }
+
+      setPokemon(temp);
     };
 
     fetchData().catch(console.error);
