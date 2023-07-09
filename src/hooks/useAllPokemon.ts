ddc02a0e4e8c87d6f1202972685726c1ca417853
Awad <34748292+Awaddd@users.noreply.github.com>
2023-07-09 17:58:04 +0100

    Update names to avoid confusion and unexpected behaviour

--- src/hooks/useAllPokemon.ts
+++ src/hooks/useAllPokemon.ts
@@ -10,11 +10,11 @@ export const useAllPokemon = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      let allPokemon: AllPokemon[] = [];
+      let pokemonArr: AllPokemon[] = [];
 
       const cached = await localforage.getItem<AllPokemon[]>("all_pokemon");
 
-      if (cached) allPokemon = cached;
+      if (cached) pokemonArr = cached;
       else {
         const { data } = await axios.get<{ results: AllPokemonDTO[] }>(
           allPokemonUrl
@@ -22,13 +22,13 @@ export const useAllPokemon = () => {
 
         data.results.forEach((pokemon) => {
           const item = AllPokemonMapper(pokemon);
-          allPokemon.push(item);
+          pokemonArr.push(item);
         });
 
-        await localforage.setItem("all_pokemon", allPokemon);
+        await localforage.setItem("all_pokemon", pokemonArr);
       }
 
-      setAllPokemon(allPokemon);
+      setAllPokemon(pokemonArr);
     };
 
     fetchData().catch(console.error);
